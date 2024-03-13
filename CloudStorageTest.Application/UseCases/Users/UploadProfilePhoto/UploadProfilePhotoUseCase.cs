1763a30b0f8352d8548d18e0f64b16dcc2532728
5935963 <anaqueiroz.dev@gmail.com>
2024-03-13 13:14:02 -0300

    Adicionar arquivos de projeto.

---
+++ CloudStorageTest.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -0,0 +1,9 @@
+﻿//UseCase é padrão para guardar as regras de nogocio, assim evitando que essas fiquem dentro da controller
+namespace CloudStorageTest.Application.UseCases.Users.UploadProfilePhoto;
+public class UploadProfilePhotoUseCase
+{
+    public void Execute()
+    {
+        
+    }
+}
